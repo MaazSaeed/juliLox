85b5a1a07d9e58e979a0fa7e57908fdf1b3ca02c
Maaz Saeed <116943667+MaazSaeed@users.noreply.github.com>
2024-11-04 23:26:39 +0500

    Scanning operators

--- Lexer.jl
+++ Lexer.jl
@@ -15,6 +15,8 @@ function scan_tokens(lexer::Lexer)
         lexer.start = lexer.current
         scan_token(lexer)
     end
+
+    push!(lexer.tokens, Token(EOF, "", nothing, lexer.line))
 end
 
 function scan_token(lexer::Lexer)
@@ -31,8 +33,21 @@ function scan_token(lexer::Lexer)
         '+' => add_token(lexer, PLUS)
         ';' => add_token(lexer, SEMICOLON)
         '*' => add_token(lexer, STAR)
-        _   => nothing 
+        '!' => add_token(lexer, match(lexer, '=') ? BANG_EQUAL : BANG)
+        '=' => add_token(lexer, match(lexer, '=') ? EQUAL_EQUAL : EQUAL)
+        '<' => add_token(lexer, match(lexer, '=') ? LESS_EQUAL : LESS)
+        '>' => add_token(lexer, match(lexer, '=') ? GREATER_EQUAL : GREATER)
+
+        _   => error("Unexpected character on line.")
     end
+end
+
+function match(lexer::Lexer, expected::Char)
+    if is_at_end(lexer) return false end
+    if lexer.source[lexer.current] != expected return false end
+
+    advance!(lexer) # Consume the character
+    return true
 end
 
 function advance!(lexer::Lexer)
@@ -50,12 +65,14 @@ function add_token(lexer::Lexer, type::TokenType)
 end
 
 function add_token(lexer::Lexer, type::TokenType, literal::Any)
-    text = lexer.source[lexer.start:lexer.current - 1]
+    text = lexer.source[lexer.start : lexer.current - 1]
     push!(lexer.tokens, Token(type, text, literal, lexer.line))
 end
 
+
+
 # Create an instance of Lexer with an empty tokens array
-L = Lexer("(){}*+-;.", Vector{Token}(), 1, 1, 1)
+L = Lexer("!=,==,>=,<=", Vector{Token}(), 1, 1, 1)
 
 # Scan tokens
 scan_tokens(L)
