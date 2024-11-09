5beacfdd251a7bebca0ffaa25fb676155c94800e
Maaz Saeed <116943667+MaazSaeed@users.noreply.github.com>
2024-11-09 23:07:59 +0500

    Recursive descent parsing

--- Parser.jl
+++ Parser.jl
@@ -24,16 +24,108 @@ end
 
 function match(parser::Parser, types...)
     for type in types 
-        if check(type)
+        if check(parser, type)
+            advance(parser)
+            return true
+        end
+    end
+
+    false
 end
 
-function check(parser::Parser) end
-function advance(parser::Parser) end
-function is_at_end(parser::Parser) end
-function peek(parser::Parser) end
-function previous(parser::Parser) end
-function comparison(parser::Parser) end
-function term(parser::Parser) end
-function factor(parser::Parser) end
-function unary(parser::Parser) end
-function primary(parser::Parser) end
+function check(parser::Parser, type::TokenType) 
+    if is_at_end(parser)
+        return false
+    end
+
+    peek(parser).type == type
+end
+
+function advance(parser::Parser) 
+    if !is_at_end(parser)
+        parser.current += 1
+    end
+
+    previous(parser)
+end
+
+
+function is_at_end(parser::Parser)
+    peek(parser).type == EOF 
+end
+
+function peek(parser::Parser) 
+    parser.tokens[parser.current]
+end
+
+
+function previous(parser::Parser) 
+    parser.tokens[parser.current - 1]
+end
+
+function comparison(parser::Parser) 
+    expr = term(parser)
+
+    while match(parser, GREATER, GREATER_EQUAL, LESS, LESS_EQUAL)
+        operator = previous(parser)
+        right = term(parser)
+        expr = Binary(expr, operator, right)
+    end
+
+    expr
+end
+
+function term(parser::Parser) 
+    expr = factor(parser)
+
+    while match(parser, MINUS, PLUS)
+        operator = previous(parser)
+        right = factor(parser)
+        expr = Binary(epxr, operator, right)
+    end
+
+    expr 
+end
+
+
+function factor(parser::Parser) 
+    expr = unary(parser)
+
+    while match(parser, SLASH, STAR)
+        operator = previous(parser)
+        right = unary(parser)
+        expr = Binary(expr, operator, right)
+    end
+
+    expr 
+end
+
+function unary(parser::Parser) 
+    if match(parser, BANG, MINUS)
+        operator = previous(parser)
+        right = unary(parser)
+        return Unary(operator, right)
+    end
+
+    primary(parser)
+end
+
+function primary(parser::Parser) 
+    if match(parser, FALSE) return Literal(false) end
+    if match(parser, TRUE) return Literal(true) end
+    if match(parser, NIL) return Literal(nothing) end
+    
+    if match(parser, NUMEBR, STRING)
+        return Literal(previous(parser).literal)
+    end
+
+    if match(parser, LEFT_PAREN)
+        expr = expression(parser)
+        consume(parser, RIGHT_PAREN, "Expect ')' after expression.")
+        Grouping(expr)
+
+        return expr
+    end
+end
+
+function consume(parser::Parser, type::Token, msg::String) end
